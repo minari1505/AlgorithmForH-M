c4ec3c70fca9726ece9d9f6f0f05d7d3c1856927
minari1505 <minari1505@naver.com>
2021-08-13 20:16:53 +0900

    Solve the 11650,1978,2108

---
+++ Class2/11650/minari - 11650.py
@@ -0,0 +1,45 @@
+# 좌표 정렬하기
+
+```python
+#2차원 배열
+#1. 2차원 배열로 입력받기
+#2. lst[i][1]을 오름차순으로 정렬
+#3. if lst[i][1] == lst[i+1][1] , lst[][1]을 오름차순으로 정렬
+
+N = int(input())
+#1.
+lst = []
+for i in range(N):
+    x,y = map(int,input().split())
+    lst.append([x,y])
+#2,3
+lst.sort()
+
+for i in range(len(lst)):
+    print("{0} {1}".format(lst[i][0],lst[i][1]))
+
+```
+
+### 시간초과
+
+```python
+#2차원 배열
+#1. 2차원 배열로 입력받기
+#2. lst[i][1]을 오름차순으로 정렬
+#3. if lst[i][1] == lst[i+1][1] , lst[][1]을 오름차순으로 정렬
+import sys
+N = int(input())
+#1.
+lst = []
+for i in range(N):
+    x,y = map(int,sys.stdin.readline().split())
+    lst.append([x,y])
+#2,3
+lst.sort()
+
+for i in range(len(lst)):
+    print("{0} {1}".format(lst[i][0],lst[i][1]))
+```
+
+### 통과(import sys , stdin.leadline() 이용)
+
