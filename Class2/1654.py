ad157547955607585afc9e63f3feac3a7bb14eec
tribbless <tribbless@gmail.com>
2021-08-27 18:31:31 +0900

    1654 clear

--- Class2/1654.py
+++ Class2/1654.py
@@ -1,4 +1,17 @@
 import sys
 K, N = map(int,sys.stdin.readline().split())
-lan = list(sys.stdin.readline() for i in range(K))
-print(lan)
+lan = list(int(sys.stdin.readline()) for i in range(K))
+
+start, end = 1, max(lan)
+
+while start <= end:
+    mid = (start + end)//2
+    tmp=0
+    for x in lan:
+        tmp += x // mid
+    if tmp < N:
+        end = mid-1
+    elif tmp >= N:
+        start = mid+1
+
+print(end)
